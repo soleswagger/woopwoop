c70fca639e5ac84b271fec574643de2defb6d627
soleswagger <[valid-email]>
2025-03-13 08:59:56 -1000

    database stuff

--- whoopwhoop/src/server/db/index.ts
+++ whoopwhoop/src/server/db/index.ts
@@ -1,13 +1,22 @@
+import { drizzle } from 'drizzle-orm/vercel-postgres';
+import { sql } from "@vercel/postgres";
+import * as schema from "./schema";
+
+// Use this object to send drizzle queries to your DB
+export const db = drizzle(sql, {schema});
+
+
+/*
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
 import { env } from "~/env";
 import * as schema from "./schema";
 
-/**
+
  * Cache the database connection in development. This avoids creating a new connection on every HMR
  * update.
- */
+
 const globalForDb = globalThis as unknown as {
   conn: postgres.Sql | undefined;
 };
@@ -16,3 +25,4 @@ const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
 if (env.NODE_ENV !== "production") globalForDb.conn = conn;
 
 export const db = drizzle(conn, { schema });
+*/
